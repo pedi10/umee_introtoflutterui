bc0778d65640e5a94fe91172435be81aec048404
Pedram Moshrii <65490739+pedi10@users.noreply.github.com>
2025-01-05 16:41:30 +0800

    Update main.dart

--- Final Codes/main.dart
+++ Final Codes/main.dart
@@ -1,16 +1,9 @@
 // ignore_for_file: prefer_const_constructors, unused_import
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_workshop/dashboard.dart';
-import 'package:flutter_workshop/firebase_options.dart';
 import 'login.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
